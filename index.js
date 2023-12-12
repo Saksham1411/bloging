f3a6ccbe9536f85add224d6bea0de04b218935ea
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-12-12 22:49:47 +0530

    create and showing blog

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
 const { connectDB } = require('./connect');
 const cookieParser = require('cookie-parser');
 const { checkForAuth } = require('./middlewares/authentication');
+const path = require('path');
+const Blog = require('./models/blog');
 
 const app = express();
 const PORT = 5000;
 
-app.set("view engine","ejs");
-app.set("views","./views");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extented: false }));
 app.use(cookieParser());
 app.use(checkForAuth("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get("/",(req,res)=>{
-    res.render("Home",{
-        user : req.user,
+app.get("/",async (req, res) => {
+    const allBlogs = await Blog.find({}).sort("createdAt");
+    res.render("Home", {
+        user: req.user,
+        blogs: allBlogs,
     });
 })
 
-app.use('/user',userRoute);
+app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 const start = async () => {
     try {
---
+++ models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    coverImageURL: {
+        type: String,
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true });
+
+module.exports =  mongoose.model("Blog",blogSchema);
