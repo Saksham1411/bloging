3a510108c06a55a6931232025e371773407bf32e
Saksham1411 <sakshamsamgarg1411@gmail.com>
2023-12-26 00:05:16 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require('express');
 const userRoute = require('./routes/user');
 const blogRoute = require('./routes/blog');
-const { connectDB } = require('./connect');
+const connectDB = require('./connect');
 const cookieParser = require('cookie-parser');
 const { checkForAuth } = require('./middlewares/authentication');
 const path = require('path');
 const Blog = require('./models/blog');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -32,7 +33,7 @@ app.use('/blog', blogRoute);
 
 const start = async () => {
     try {
-        await connectDB("mongodb://127.0.0.1:27017/blogify");
+        await connectDB(process.env.MONGO_URI);
         app.listen(PORT, () => console.log('server is running...'));
     } catch (error) {
         console.log(error);
--- connect.js
+++ connect.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectDB = async(url)=>{
+const connectDB = (url)=>{
     return mongoose.connect(url);
 }
 
-module.exports = {
-    connectDB,
-}
+module.exports = connectDB
